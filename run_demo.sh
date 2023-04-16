b3f0cf197d2694cad27eb3e02459576d5254e11e
Han Zhang <micohan@slicomex.cs.washington.edu>
2023-04-16 14:07:00 -0700

    created two .sh files for endterm and weekly evaluations

--- run_demo.sh
+++
@@ -1,6 +0,0 @@
-# python evaluation/model_train_eval.py --config_name=ml_chikersal --pred_target=dep_weekly --eval_task=single
-# python evaluation/model_train_eval.py --config_name=ml_xu_interpretable --pred_target=dep_weekly --eval_task=single
-
-python evaluation/model_train_eval.py --config_name=ml_chikersal --pred_target=dep_endterm --eval_task=single
-python evaluation/model_train_eval.py --config_name=ml_wang --pred_target=dep_endterm --eval_task=single
-python evaluation/model_train_eval.py --config_name=ml_xu_interpretable --pred_target=dep_endterm --eval_task=single
